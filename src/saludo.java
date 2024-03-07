cb4366d0e19d379ace529b914578b34746f5d970
gerar <gerardo.saucedo2238@uppuebla.edu.mx>
2024-03-07 13:00:55 -0600

    Metodo saludo

---
+++ src/saludo.java
@@ -0,0 +1,17 @@
+public class saludo
+{
+    private String saludar;
+
+    public String getSaludar() {
+        return saludar;
+    }
+
+    public void setSaludar(String saludar) {
+        //this.saludar = saludar;
+    }
+
+    public saludo()
+    {
+
+    }
+}
